cbadae99b2303ec6ab486061a74f949955c24100
Johnathon Rexus <22.johnathon.rexus@ksd.org>
2021-05-04 09:58:20 -0700

    Please work

---
+++ 3D Testing/Assets/EnemyMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour
+{
+    public GameObject player;
+    NavMeshAgent myNavMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myNavMeshAgent.SetDestination(player.transform.position);
+    }
+}
