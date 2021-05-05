4a29d86bc14e5d5bad5bf865857bbbe8f0ad118d
Johnathon Rexus <22.johnathon.rexus@ksd.org>
2021-05-05 09:50:05 -0700

    Fix Enemy AI. Get it to move properly, follow paths, see obstructions, etc.

--- 3D Testing/Assets/EnemyMove.cs
+++ 3D Testing/Assets/EnemyMove.cs
@@ -7,15 +7,27 @@ public class EnemyMove : MonoBehaviour
 {
     public GameObject player;
     NavMeshAgent myNavMeshAgent;
+    public Transform[] points;
+    int currentPoint = 0;
     // Start is called before the first frame update
     void Start()
     {
         myNavMeshAgent = GetComponent<NavMeshAgent>();
+        myNavMeshAgent.SetDestination(points[0].position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        myNavMeshAgent.SetDestination(player.transform.position);
+       // myNavMeshAgent.SetDestination(player.transform.position);
+       if(myNavMeshAgent.remainingDistance < 0.5f)
+        {
+            GoToNextPoint();
+        }
+    }
+    void GoToNextPoint()
+    {
+        currentPoint = (currentPoint + 1) % points.Length;
+        myNavMeshAgent.SetDestination(points[currentPoint].position);
     }
 }
---
+++ 3D Testing/Assets/PlayerInteract.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, transform.forward, out hit);
+        Debug.Log(hit.transform.gameObject.name);
+    }
+}
