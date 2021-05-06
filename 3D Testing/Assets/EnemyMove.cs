0e7f9c7000c34006989bdec84605ba83a821c6cb
Johnathon Rexus <22.johnathon.rexus@ksd.org>
2021-05-06 10:15:49 -0700

    Enemy issue; doesn't follow paths OR player

--- 3D Testing/Assets/EnemyMove.cs
+++ 3D Testing/Assets/EnemyMove.cs
@@ -7,27 +7,40 @@ public class EnemyMove : MonoBehaviour
 {
     public GameObject player;
     NavMeshAgent myNavMeshAgent;
-    public Transform[] points;
-    int currentPoint = 0;
+    public Transform[] waypoints;
+    int currentWaypoint = 0;
+    public float chaseRange = 10.0f;
+    bool isChasing = false;
+
     // Start is called before the first frame update
     void Start()
     {
         myNavMeshAgent = GetComponent<NavMeshAgent>();
-        myNavMeshAgent.SetDestination(points[0].position);
+        myNavMeshAgent.SetDestination(waypoints[0].position);
     }
 
     // Update is called once per frame
     void Update()
     {
-       // myNavMeshAgent.SetDestination(player.transform.position);
-       if(myNavMeshAgent.remainingDistance < 0.5f)
+        // myNavMeshAgent.SetDestination(player.transform.position);
+       float distance = Vector3.Distance(player.transform.position, transform.position);
+       if(distance < chaseRange)
         {
-            GoToNextPoint();
+            myNavMeshAgent.SetDestination(player.transform.position);
+            isChasing = true;
+        }else if(isChasing)
+        {
+            myNavMeshAgent.SetDestination(waypoints[currentWaypoint].position);
+            isChasing = false;
+        }
+       if(myNavMeshAgent.remainingDistance < 0.5f && !isChasing)
+        {
+            GoToNextWayPoint();
         }
     }
-    void GoToNextPoint()
+    void GoToNextWayPoint()
     {
-        currentPoint = (currentPoint + 1) % points.Length;
-        myNavMeshAgent.SetDestination(points[currentPoint].position);
+        currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+        myNavMeshAgent.SetDestination(waypoints[currentWaypoint].position);
     }
 }
